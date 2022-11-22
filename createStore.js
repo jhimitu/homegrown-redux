58a499d2c20a3620a2c182e0a22861d79411ba6a
Jhia Turner <jhimitu@gmail.com>
2022-11-22 06:21:50 -0800

    add unsubscribe function to subscribe method

--- createStore.js
+++ createStore.js
@@ -12,6 +12,12 @@ function createStore(reducer, initialState) {
 
     function subscribe(listener) {
         subscribers.push(listener);
+        return function unsubscribe() {
+            // get the index of the subscriber that was passed in
+            const index = subscribers.indexOf(listener);
+            // remove that subscriber from the list
+            subscribers.splice(index, 1);
+        }
     }
 
     return { getState, dispatch, subscribe };
