be9081f573c8c2ca50e96771820bf4daf626bf10
Jhia Turner <jhimitu@gmail.com>
2022-11-22 06:29:25 -0800

    add comments, cleanup

--- combineReducers.js
+++ combineReducers.js
@@ -1,7 +1,12 @@
+// takes in an object of reducers
 function combineReducers(reducers) {
+    // skeleton reducer - a function call with state and an action
     return (state = {}, action) => {
+        // the new state we want to return after calling all reducers
         const newState = {};
+        // call each reducer and set its value in the new state object
         for (let reducer in reducers) {
+            // this state[reducer] is initially undefined when passed in
             newState[reducer] = reducers[reducer](state[reducer], action)
         }
         return newState;
